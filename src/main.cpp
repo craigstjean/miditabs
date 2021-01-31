e661f3a51137ba94e112aaf3cce1a0b1bc9552eb
Craig St. Jean <craig@frozenrosesoftware.com>
2021-01-31 18:04:25 -0500

    Initial setup

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <MidiFile.h>
+#include <Options.h>
+
+int main(int argc, const char **argv)
+{
+    smf::Options options;
+    std::cout << "test";
+    return 0;
+}
+
