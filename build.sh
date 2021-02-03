dbe423061f5ad4ca68570a66f152ed8b615b1902
Craig St. Jean <craig@frozenrosesoftware.com>
2021-02-03 00:50:32 -0500

    Add ANTLR4 download to build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
+if [ ! -f deps/antlr-4.9.1-complete.jar ]; then
+    curl https://www.antlr.org/download/antlr-4.9.1-complete.jar -o deps/antlr-4.9.1-complete.jar
+fi
+
 make antlr
 mkdir -p build
 cd build
